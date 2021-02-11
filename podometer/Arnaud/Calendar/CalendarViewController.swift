e3fc8d588c02b8a94a6b8355042706b6ee94b33b
Watliel <arnaud.salomon20@gmail.com>
2021-02-11 14:25:33 +0100

    adding calendar view

--- podometer/Arnaud/Calendar/CalendarViewController.swift
+++ podometer/Arnaud/Calendar/CalendarViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import CoreMotion
 
 class CalendarViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var stepLabel: UILabel!
     @IBOutlet weak var collection_view: UICollectionView!
+    @IBOutlet weak var distLabel: UILabel!
     
     class func newInstance(calendar: Calendar) -> CalendarViewController {
         let vc = CalendarViewController()
@@ -25,37 +28,49 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
         collection_view.delegate = self
         collection_view.dataSource = self
         
-        
-        let user = User.all.first
-        print("OOOOOOOOOHHHHHHHHHHH")
-        
+  
+        print(Date.getInstallationDate())
         
         let bundle = Bundle(for: type(of: self))
         let nib = UINib(nibName: "CalendarCollectionViewCell", bundle: bundle)
-        collection_view.register(nib, forCellWithReuseIdentifier: "COLLECTION_CELL_CALENDAR_IDENTIFER")
         
+        collection_view.register(nib, forCellWithReuseIdentifier: "COLLECTION_CELL_CALENDAR_IDENTIFER")
+        let podo = PodometerModel.todayData()
+        if(podo != nil) {
+            setLabelValue(podometer: podo!)
+        }
+        
+                
         // Do any additional setup after loading the view.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let dateUser = Date.getInstallationDate()
-        let date = Date()
-
-       // let dateComp = date.distanceBetween(dateUser)
-        return 10//dateComp
-
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
        let cell = collection_view.dequeueReusableCell(withReuseIdentifier: "COLLECTION_CELL_CALENDAR_IDENTIFER", for: indexPath) as! CalendarCollectionViewCell
         
         
-        
-        
-        //cell.dayLabel.height(755.0)
-        
         return cell
     }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let dayBefore = datePastDays(numberOfDayBefore: -indexPath.row)
+        
+        
+    }
+    
+    func getDataPodometer(date: Date) -> PodometerModel {
+        return PodometerModel.data(startDate: Date.getInstallationDate(), endDate: date)!
+    }
 
-
+    func datePastDays(numberOfDayBefore: Int) -> Date {
+        let calendar = Calendar.current
+        return calendar.date(byAdding: .day, value: -numberOfDayBefore, to: Date())!
+    }
+    
+    func setLabelValue(podometer: PodometerModel) ->Void{
+        distLabel.text = podometer.distence.stringValue
+        stepLabel.text = podometer.step.stringValue
+    }
 }
